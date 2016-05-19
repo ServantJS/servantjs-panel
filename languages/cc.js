6edb1c89eccb471cae12743f126402b6a3aeb171
Vitaly Orlov <vitaliuorlov@gmail.com>
2016-05-20 00:00:41 +0300

    add

---
+++ languages/cc.js
@@ -0,0 +1,52 @@
+exports.ru = {
+    info: {
+        title: 'Командный центр',
+        pageTitle: 'Командный центр',
+
+        dt: 'Дата',
+        status: 'Статус',
+        ip: 'IP',
+
+        serverName: 'Сервер',
+        sysName: 'Системное имя',
+        modules: 'Модули',
+
+        agents: 'Агенты'
+    },
+
+    groups: {
+        title: 'Список групп агентов',
+        pageTitle: 'Список групп агентов',
+
+        table: {
+            name: 'Группы',
+            columns: {
+                name: 'Наименование',
+                cc: 'Целевой сервер',
+                sysName: 'Системное имя',
+                agents: 'Агенты'
+            }
+        },
+
+        modal: {
+            title: 'Создание новой группы',
+            fields: {
+                name: 'Наименование',
+                ccServer: 'Целевой сервер',
+                agents: 'Агенты'
+            }
+        }
+    }
+};
+
+exports.us = {
+    info: {
+        title: 'Command center',
+        pageTitle: 'Command center'
+    },
+
+    groups: {
+        title: 'Agents\' group list',
+        pageTitle: 'Agents\' group list'
+    }
+};
