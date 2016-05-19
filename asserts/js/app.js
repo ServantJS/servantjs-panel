3d683472a119df465c11f7381489b7ba44c628ab
Vitaly Orlov <vitaliuorlov@gmail.com>
2016-05-19 23:59:21 +0300

    add toastr options

--- asserts/js/app.js
+++ asserts/js/app.js
@@ -26,4 +26,19 @@ $(document).ready(function () {
                 .toggleClass('fa-angle-left')
                 .toggleClass('fa-angle-down');
     });
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "positionClass": 'toast-top-right',
+        "onclick": null,
+        "showDuration": 1000,
+        "hideDuration": 1000,
+        "timeOut": 7000,
+        "extendedTimeOut": 1000,
+        "showEasing": 'swing',
+        "hideEasing": 'linear',
+        "showMethod": 'fadeIn',
+        "hideMethod": 'fadeOut'
+    };
 });
