6dec626fc46d81d592a096243cfeddf7d608f6b1
Vitaly Orlov <vitaliuorlov@gmail.com>
2016-05-20 17:18:14 +0300

    created

---
+++ languages/task.js
@@ -0,0 +1,33 @@
+exports.ru = {
+    index: {
+        title: 'Информация по заданию',
+        pageTitle: 'Информация по заданию',
+
+        dt: 'Дата\\время',
+        status: 'Состояние',
+        cmd: 'Операция',
+        task: 'Задание',
+        userName: 'Имя пользователя',
+        module: 'Модуль',
+        innerError: 'Внутренняя ошибка',created
+        report: 'Отчет',
+        error: 'Ошибки'
+    }
+};
+
+exports.us = {
+    index: {
+        title: 'Task information',
+        pageTitle: 'Task information',
+
+        dt: 'Date\\time',
+        status: 'State',
+        cmd: 'Operation',
+        task: 'Task',
+        userName: 'Username',
+        module: 'Module',
+        innerError: 'Internal error',
+        report: 'Report',
+        error: 'Errors'
+    }
+};
