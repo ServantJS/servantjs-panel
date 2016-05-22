793d51b96560c34037d11669262cdec3ad9f4128
Vitaly Orlov <vitaliuorlov@gmail.com>
2016-05-22 19:51:42 +0300

    update

--- languages/user.js
+++ languages/user.js
@@ -7,36 +7,13 @@ exports.ru = {
             create: 'Добавить пользователя'
         },
 
-        modal: {
-            edit: {
-                title: 'Редактирование пользователя',
-                roles: 'Роли',
-                groups: 'Группы'
-            }
-        },
-
-        confirm: {
-            title: 'Подверждение',
-            text: 'Вы уверены, что хотите удалить пользователя?'
-        },
-
         table: {
             name: 'Пользователи',
             columns: {
                 email: 'Эл. почта',
                 name: 'Имя',
-                groups: 'Группы',
-                roles: 'Роли'
+                token: 'API ключ'
             }
-        }
-    },
-
-    ajax: {
-        user: {
-            deleted: 'Пользователь успешно удален',
-            rootDel: 'Нельзя удалить root пользователя',
-            rootEdit: 'Нельзя редактировать root пользователя',
-            edit: 'Пользователь успешно изменен'
         }
     }
 };
@@ -50,36 +27,13 @@ exports.us = {
             create: 'Create user'
         },
 
-        modal: {
-            edit: {
-                title: 'Edit user',
-                roles: 'Roles',
-                groups: 'Groups'
-            }
-        },
-
-        confirm: {
-            title: 'Confirm',
-            text: 'Are you sure that you want to remove user?'
-        },
-
         table: {
             name: 'Users',
             columns: {
                 email: 'Email',
                 name: 'Name',
-                groups: 'Groups',
-                roles: 'Roles'
+                token: 'API token'
             }
-        }
-    },
-
-    ajax: {
-        user: {
-            deleted: ' The user is successfully deleted',
-            rootDel: 'It is not possible to delete root account',
-            rootEdit: 'It is not possible to edit root account',
-            edit: 'The user is successfully updated'
         }
     }
 };
