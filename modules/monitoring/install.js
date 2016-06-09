3b57f9b681af806eb70497aa46369d7c7c188ca0
Vitaly Orlov <vitaliuorlov@gmail.com>
2016-06-09 12:05:20 +0300

    add new metrics

--- modules/monitoring/install.js
+++ modules/monitoring/install.js
@@ -23,10 +23,70 @@ module.exports = () => {
                     ru: lang.ru.db.systemModule.metric.cpu.desc,
                     us: lang.us.db.systemModule.metric.cpu.desc
                 },
-
-                settings: []
+                view_order: 0,
+                isDetail: false,
+                settings: [],
+                events: [{
+                    sys_name: 'on_normal',
+                    name: {
+                        ru: lang.ru.db.systemModule.metric.cpu.event.on_normal,
+                        us: lang.us.db.systemModule.metric.cpu.event.on_normal
+                    }
+                }, {
+                    sys_name: 'on_warning',
+                    name: {
+                        ru: lang.ru.db.systemModule.metric.cpu.event.on_warning,
+                        us: lang.us.db.systemModule.metric.cpu.event.on_warning
+                    }
+                }, {
+                    sys_name: 'on_critical',
+                    name: {
+                        ru: lang.ru.db.systemModule.metric.cpu.event.on_critical,
+                        us: lang.us.db.systemModule.metric.cpu.event.on_critical
+                    }
+                }]
             })).save((err) => {
-               cb(err, [id]); 
+                cb(null, [id]);
+            });
+        },
+        (metrics, cb) => {
+            const id = new mongoose.Types.ObjectId();
+            metrics.push(id);
+            (new moduleDB.MetricModel({
+                _id: id,
+                sys_name: 'os_ram',
+                name: {
+                    ru: lang.ru.db.systemModule.metric.ram.name,
+                    us: lang.us.db.systemModule.metric.ram.name
+                },
+                description: {
+                    ru: lang.ru.db.systemModule.metric.ram.desc,
+                    us: lang.us.db.systemModule.metric.ram.desc
+                },
+                view_order: 1,
+                isDetail: false,
+                settings: [],
+                events: [{
+                    sys_name: 'on_normal',
+                    name: {
+                        ru: lang.ru.db.systemModule.metric.cpu.event.on_normal,
+                        us: lang.us.db.systemModule.metric.cpu.event.on_normal
+                    }
+                }, {
+                    sys_name: 'on_warning',
+                    name: {
+                        ru: lang.ru.db.systemModule.metric.cpu.event.on_warning,
+                        us: lang.us.db.systemModule.metric.cpu.event.on_warning
+                    }
+                }, {
+                    sys_name: 'on_critical',
+                    name: {
+                        ru: lang.ru.db.systemModule.metric.cpu.event.on_critical,
+                        us: lang.us.db.systemModule.metric.cpu.event.on_critical
+                    }
+                }]
+            })).save((err) => {
+                cb(null, metrics);
             });
         },
         (metrics, cb) => {
@@ -40,10 +100,47 @@ module.exports = () => {
                     ru: lang.ru.db.systemModule.desc,
                     us: lang.us.db.systemModule.desc
                 },
+
                 type: lang.ru.db.systemModule.type,
                 metrics: metrics
             })).save((err) => {
-                cb(err);
+                cb(null);
+            });
+        },
+        (cb) => {
+            const id = new mongoose.Types.ObjectId();
+            (new moduleDB.MetricModel({
+                _id: id,
+                sys_name: 'node_details',
+                name: {
+                    ru: lang.ru.db.nodeDetailsModule.metric.details.name,
+                    us: lang.us.db.nodeDetailsModule.metric.details.name
+                },
+                description: {
+                    ru: lang.ru.db.nodeDetailsModule.metric.details.desc,
+                    us: lang.us.db.nodeDetailsModule.metric.details.desc
+                },
+                isDetail: true,
+                settings: []
+            })).save((err) => {
+                cb(null, [id]);
+            });
+        },
+        (metrics, cb) => {
+            (new moduleDB.MonitoringModuleModel({
+                sys_name: 'node',
+                name: {
+                    ru: lang.ru.db.nodeDetailsModule.name,
+                    us: lang.us.db.nodeDetailsModule.name
+                },
+                description: {
+                    ru: lang.ru.db.nodeDetailsModule.desc,
+                    us: lang.us.db.nodeDetailsModule.desc
+                },
+                type: lang.ru.db.nodeDetailsModule.type,
+                metrics: metrics
+            })).save((err) => {
+                cb(null);
             });
         },
         (cb) => {
@@ -59,7 +156,7 @@ module.exports = () => {
                 },
                 type: lang.ru.db.HAProxyModule.type
             })).save((err) => {
-                cb(err);
+                cb(null);
             });
         }
     ], (err) => {
