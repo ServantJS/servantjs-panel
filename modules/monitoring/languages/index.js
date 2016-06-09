520422ba509c3fbcd479ae0e1d98511eb08b1868
Vitaly Orlov <vitaliuorlov@gmail.com>
2016-06-09 12:06:23 +0300

    update

--- modules/monitoring/languages/index.js
+++ modules/monitoring/languages/index.js
@@ -7,7 +7,32 @@ exports.ru = {
             metric: {
                 cpu: {
                     name: 'Процессор',
-                    desc: 'Информация о загрузке центрального процессора.'
+                    desc: 'Информация о загрузке центрального процессора.',
+                    event: {
+                        on_normal: 'Возврат к норме',
+                        on_warning: 'Превышение предупреждающего порога',
+                        on_critical: 'Превышение критического порога'
+                    }
+                },
+                ram: {
+                    name: 'Память',
+                    desc: 'Информация об использование оперативной памяти.',
+                    event: {
+                        on_normal: 'Возврат к норме',
+                        on_warning: 'Превышение предупреждающего порога',
+                        on_critical: 'Превышение критического порога'
+                    }
+                }
+            }
+        },
+        nodeDetailsModule: {
+            name: 'Информация об узле',
+            desc: 'Содержит метрики для сбора общей информации об узле, на котором работает агент.',
+            type: 'node',
+            metric: {
+                details: {
+                    name: 'Детальная информация',
+                    desc: 'Общая онформация об узле, с которого собираются данные.'
                 }
             }
         },
@@ -17,25 +42,25 @@ exports.ru = {
             type: 'app'
         }
     },
-    
+
     settings: {
         title: 'Настройка мониторинга',
         pageTitle: 'Настройка мониторинга',
         panelTitle: 'Настройки метрик для сервера',
-        
+
         settingsTitle: 'Настройки для метрик',
         emptySettings: 'Для данного сервера отсутствуют настройки. Введите и сохраните значения для активации метрики.',
-        
+
         save: {
             success: 'Настройки для метрики успешно сохранены.',
             notModify: 'Для метрики не обнаружены какие-либо изменения.'
         },
-        
+
         emptyMetrics: '<string>Внимание!</string> Для данного модуля отсутстуют настройки метрик.',
-        
+
         metricName: 'Метрика',
         serverName: 'Сервер',
-        
+
         interval: 'Интервал (мин.):',
         repeat: 'Кол-во повторений для срабатывания:',
         isActive: 'Активирован',
@@ -43,6 +68,37 @@ exports.ru = {
             name: 'Пороги',
             warning: 'Предупреждающий:',
             critical: 'Критический:'
+        },
+        events: 'События'
+    },
+
+    servers: {
+        title: 'Мониторинг серверов',
+        pageTitle: 'Мониторинг серверов',
+
+        agentChooseTitle: 'Выберите агента на сервере',
+        agent: 'Агент',
+        selectAgent: '--Выберите агента--',
+
+        commonModule: {
+            emptyData: 'Данные отсутствуют'    
+        },
+        
+        nodeModule: {
+            os: 'Операционная система',
+            version: 'Версия',
+            platform: 'Платформа',
+            arch: 'Архитектура',
+            kernel: 'Ядро',
+            uptime: 'Последний запуск',
+            inet: 'Интерфейс по умолчанию',
+            host: 'Имя машины',
+            ip: 'IP',
+            mac: 'MAC'
+        },
+
+        osModule: {
+            
         }
     }
 };
@@ -56,7 +112,32 @@ exports.us = {
             metric: {
                 cpu: {
                     name: 'CPU',
-                    desc: 'CPU usage information.'
+                    desc: 'CPU usage information.',
+                    event: {
+                        on_normal: 'Return to normal',
+                        on_warning: 'Exceeding the warning threshold',
+                        on_critical: 'Exceeding the critical threshold'
+                    }
+                },
+                ram: {
+                    name: 'RAM',
+                    desc: 'RAM usage information.',
+                    event: {
+                        on_normal: 'Return to normal',
+                        on_warning: 'Exceeding the warning threshold',
+                        on_critical: 'Exceeding the critical threshold'
+                    }
+                }
+            }
+        },
+        nodeDetailsModule: {
+            name: 'Node details',
+            desc: 'It contains the metrics to collect general information about the node where the agent is running.',
+            type: 'node',
+            metric: {
+                details: {
+                    name: 'Details',
+                    desc: 'Summary information about node.'
                 }
             }
         },
