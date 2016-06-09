224f861be70a8f5af48f2cf7bec59a2797953f34
Vitaly Orlov <vitaliuorlov@gmail.com>
2016-06-09 12:05:47 +0300

    add new models

--- modules/monitoring/db.js
+++ modules/monitoring/db.js
@@ -6,3 +6,8 @@ require('./models').load();
 
 exports.MonitoringModuleModel = mongoose.model('MonitoringModule');
 exports.MetricModel = mongoose.model('Metric');
+exports.CPUHistoryModel = mongoose.model('CPUHistory');
+exports.CPUEventModel = mongoose.model('CPUEvent');
+exports.RAMHistoryModel = mongoose.model('RAMHistory');
+exports.RAMEventModel = mongoose.model('RAMEvent');
+exports.NodeDetailsModel = mongoose.model('NodeDetails');
