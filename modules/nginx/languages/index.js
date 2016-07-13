3f4b3bdaf3eaf01908fcb2bf233e640ffc75eadf
Vitaly Orlov <vitaliuorlov@gmail.com>
2016-07-13 12:23:26 +0300

    add english words

--- modules/nginx/languages/index.js
+++ modules/nginx/languages/index.js
@@ -25,7 +25,7 @@ exports.ru = {
                 content: 'Содержимое',
                 group: 'Группа',
                 vars: 'Переменные',
-                kind: 'Исключить возможноть изменения статуса для конфига?'
+                kind: 'Исключить возможность изменения статуса для конфига?'
                 
             }
         }
@@ -102,11 +102,104 @@ exports.ru = {
 };
 
 exports.us = {
+    configs: {
+        title: 'NGINX configs',
+        pageTitle: 'Configs',
+        table: {
+            name: 'Configs list',
+            columns: {
+                name: 'Name',
+                group: 'Group',
+                target: 'Target',
+                state: 'State'
+            }
+        },
 
+        modal: {
+            title: 'Create a new config file',
+            editTitle: 'Edit the config file',
+            customTemplate: 'Custom',
+            chooseTemplate: '--Choose template--',
+            chooseGroup: '--Choose group--',
+
+            fields: {
+                name: 'Name',
+                template: 'Template',
+                content: 'Content',
+                group: 'Group',
+                vars: 'Variables',
+                kind: 'Exclude the possibility of changing the status of the config?'
+
+            }
+        }
+    },
+
+    groups: {
+        title: 'NGINX groups',
+        pageTitle: 'Configs group',
+
+        hint: {
+            name: 'Hint',
+            content: 'With the groups specified location created configuration files. The group may be linked to a specific server or group of servers. The directory, which is indicated for the group is not automatically created, so before you create, make sure that the directory exists.'
+        },
+
+        table: {
+            name: 'Список групп',
+            columns: {
+                name: 'Name',
+                target: 'Target',
+                sourceDir: 'Directory'
+            }
+        },
+
+        modal: {
+            title: 'Create a new config\'s group',
+            fields: {
+                name: 'Name',
+                target: 'Target',
+                sourceDir: 'Directory'
+            }
+        }
+    },
+
+    templates: {
+        title: 'NGINX templates',
+        pageTitle: 'Configs template',
+        table: {
+            name: 'Configs template'
+        },
+
+        item: {
+            template: 'Template',
+            vars: 'Variables'
+        },
+
+        hint: {
+            name: 'Hine',
+            content: 'Templates are useful when you have to create the same type of configuration files for nignx, which differ in a couple of settings. Each template contains a set of configurations and variables to replace them in the template.'
+        },
+
+        modal: {
+            title: 'Create a new template',
+            fields: {
+                name: 'Name',
+                content: 'Content',
+                vars: {
+                    title: 'Variables',
+                    name: 'Name',
+                    desc: 'Description',
+                    pattern: '${Pattern}',
+                    patternHint: 'Pattern: The value of the variable in the config. For example, $ {domain}',
+                    value: 'Value'
+                }
+            }
+        }
+    },
 
     ajax: {
         delete: {
-            ok: ' successfully removed'
+            ok: ' successfully removed.',
+            notOk: ' can not be removed, as there is bound configuration files.'
         }
     }
 };
